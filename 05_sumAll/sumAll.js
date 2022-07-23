8c78f3399b64209658b515a6f328cd506d433049
Brendon <brendoncesar40@gmail.com>
2022-07-23 18:54:17 -0300

    sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(x_int,y_int) {
+    let sum=0
+    if(x_int < 0|| y_int <0 || typeof(x_int)!== "number" || typeof(y_int)!== "number"){   //checagem de argumento 
+        return "ERROR"
+    }
 
+    if(x_int > y_int){
+        for (let i = y_int; i <= x_int; i++) {
+            sum+=i
+        }
+    }else{
+        for (let i = x_int; i <= y_int; i++) {
+            sum+=i
+        }
+    }
+    return sum
 };
 
 // Do not edit below this line
