e199557fe9c803a0cab772deabc5284a81fdf290
Brendon <brendoncesar40@gmail.com>
2022-07-23 20:03:16 -0300

    temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+    let celsius = (temperature-32) * (5/9)
+    return round(celsius,1)
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+    let fahrenheit = (temperature* (9/5)) +32
+    return round(fahrenheit,1)
 };
 
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 // Do not edit below this line
 module.exports = {
   ftoc,
