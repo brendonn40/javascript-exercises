a2023189c38c844911aa25e7a7ced86d1d7c19b0
Brendon-note <brendoncesa40@gmail.com>
2022-07-23 00:03:16 -0300

    hello world = repeatstring

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    if(num === 0){
+        return ""
+    }
+    if(num < 0){
+        return "ERROR"
+    }
+    result = string
+    for (let i = 1; i < num; i++) {
+        result+=string
+        
+    }
+    return result
 };
 
 // Do not edit below this line
