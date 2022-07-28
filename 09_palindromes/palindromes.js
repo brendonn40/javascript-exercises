38e120f0efbfbafc17575d2377a9de8fdacb3026
Brendon-note <brendoncesa40@gmail.com>
2022-07-27 22:12:55 -0300

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    newString = string.replaceAll(",","").replaceAll(".","").replaceAll(" ","").replaceAll("!","").toLowerCase()
+    if(newString.split("").toString() === newString.split("").reverse().toString()){
+        return true
+    }else{
+        return false
+    }
 };
 
 // Do not edit below this line
