0c2b044904baca2cd1a46285ff0ad25f5e381d4b
Brendon-note <brendoncesa40@gmail.com>
2022-07-27 21:45:14 -0300

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	result = 0
+  for (const i of array) {
+    result+=i
+  }
+  return result
+}
+  
+
+
+const multiply = function(array) {
+  result = 1
+  for (const i of array) {
+    result*=i
+  }
+  return result
 };
 
-const multiply = function() {
-
+const power = function(b,e) {
+	return b ** e
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+	if(number === 0 ){
+    return 1
+  }
+  else{
+    return number * factorial(number -1)
+  }
 };
 
 // Do not edit below this line
