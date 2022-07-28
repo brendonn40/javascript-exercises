8fc3b5e78a2ed21f6408a25a2b188df9d0bdfd51
Brendon-note <brendoncesa40@gmail.com>
2022-07-27 22:24:32 -0300

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(member) {
+    let array =[1,1]
+    if(member < 0){
+        return "OOPS"
+    }
+    for (let i = 2; i < member; i++) {
+        array.push(array[i-2]+array[i-1])
+    }
+    return array[member-1]
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
