32066babc39044f363298d492a15597b602b4581
Brendon <brendoncesar40@gmail.com>
2022-07-28 21:20:52 -0300

    tentativa findtheoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(arrayOfObj) {
+    currentYear = new Date().getFullYear()
+    let oldest = {name:"teste",yearOfBirth:currentYear,yearOfDeath:currentYear}
+    for (const {people} in arrayOfObj ){
+        if("yearOfDeath" in {people}){
+            if(people.yearOfDeath - people.yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth){
+                oldest = people
+            }
+        }else{
+            people["yearOfDeath"] = currentYear
+            if(people.yearOfDeath - people.yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth){
+                oldest = people
+        }  
+    }
+}   
+    return oldest
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
